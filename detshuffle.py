1eeca38af92690ce3a5cb48b0da03c734c1e4ffc
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-02-24 01:07:45 +0100

    Converted to zlib.compressobj thanks to lots of options. Investigations are still in progres..

--- detshuffle.py
+++ detshuffle.py
@@ -1,18 +1,22 @@
-# Shuffle a deck of cards in a deterministic way and measure entropy of such a shuffling.
+# Shuffle a deck of cards in a deterministic way and measure entropy of such shuffling.
 # Alexander Ivashkin, 2018
 
 import zlib
 import random
-
-def get_entropy(data):
-    """Calculate entropy of data by zlib'ing it"""
-    return len(zlib.compress(bytes(data), zlib.Z_BEST_COMPRESSION))
-
+import secrets
 
 # Create a non-shuffled deck of deck_size cards
-deck_size = 52000
+deck_size = 256
 deck = [i % 256 for i in range(deck_size)]
+
+def get_entropy(data):
+    """Calculate entropy of Unicode data by zlib'bing it"""
+    cmpr = zlib.compressobj(level = 9, wbits = -15, memLevel = 9, zdict = bytes(deck))
+    return len(cmpr.compress(bytes(data)) + cmpr.flush())
+
+
 ordered_deck_entropy = get_entropy(deck)
+
 # To ensure same results on each run
 random.seed(666)
 random_deck_entropy = get_entropy([round(random.random() * 255) for i in range(deck_size)])
@@ -27,7 +31,7 @@ def test_entropy(infoText, data):
 # All zeroes and random numbers (for reference)
 test_entropy("'0'", [0 for i in range(deck_size)])
 # test_entropy(str(deck_size) + " random bytes: ", )
-# test_entropy("Non-shuffled deck: ", deck)
+test_entropy("non-shuffled", deck)
 
 # Put cards into five piles
 # NB: this is not the most efficient code, but I'm writing it to learn different features of Python
@@ -38,4 +42,8 @@ for card in range(len(deck)):
 test_entropy("'into five piles'", [deck5pi[p][c] for p in range(5) for c in range(len(deck5pi[p]))])
 
 # Ten piles
-deck10pi = []
+deck10pi = [[], [], [], [], [], [], [], [], [], []]
+for card in range(len(deck)):
+    deck10pi[card % 10].append(deck[card])
+
+test_entropy("'into ten piles'", [deck10pi[p][c] for p in range(10) for c in range(len(deck10pi[p]))])
