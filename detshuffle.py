250a269d0b47b95af4cdd80a013a702d16d74781
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-02-25 01:44:42 +0100

    Added a few minor changes from twobytes

--- detshuffle.py
+++ detshuffle.py
@@ -7,31 +7,33 @@ import secrets
 
 # Create a non-shuffled deck of deck_size cards
 deck_size = 256
-deck = [i % 256 for i in range(deck_size)]
+deck = [i for i in range(deck_size)]
 
 def get_entropy(data):
-    """Calculate entropy of Unicode data by zlib'bing it"""
-    cmpr = zlib.compressobj(level = 9, wbits = -15, memLevel = 9, zdict = bytes(deck))
     return len(cmpr.compress(bytes(data)) + cmpr.flush())
+    """Calculate entropy of data by zlib'bing it"""
+    cmpr = zlib.compressobj(level = 9, wbits = -15, memLevel = 9)
 
 
 ordered_deck_entropy = get_entropy(deck)
 
 # To ensure same results on each run
 random.seed(666)
-random_deck_entropy = get_entropy([round(random.random() * 255) for i in range(deck_size)])
+random_deck_entropy = get_entropy([secrets.randbelow(deck_size) for i in range(deck_size)])
 
 def test_entropy(infoText, data):
     """Test data entropy by compressing it with zlib"""
     entropy = get_entropy(data)
     print("{} of {} cards: entropy measure {}, {:.1f}% ordered / {:.1f}% random".format(
-        deck_size, infoText, entropy, entropy / ordered_deck_entropy * 100, entropy / random_deck_entropy * 100))
+        deck_size, infoText, entropy, entropy / ordered_deck_entropy * 100,
+        entropy / random_deck_entropy * 100))
 
 
 # All zeroes and random numbers (for reference)
 test_entropy("'0'", [0 for i in range(deck_size)])
 # test_entropy(str(deck_size) + " random bytes: ", )
 test_entropy("non-shuffled", deck)
+test_entropy("random.random()", [random.randrange(0, deck_size) for i in range(deck_size)])
 
 # Put cards into five piles
 # NB: this is not the most efficient code, but I'm writing it to learn different features of Python
