44cffc84c5f205378b90b31092161db2659cbc7e
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-02-26 21:38:55 +0100

    Works. Shows. Awesome.

--- detshuffle.py
+++ detshuffle.py
@@ -1,39 +1,35 @@
-# Shuffle a deck of cards in a deterministic way and measure entropy of such shuffling.
+# Shuffle a deck of cards in a deterministic way and visualize entropy of such shuffling.
 # Alexander Ivashkin, 2018
 
-import zlib
 import random
 import secrets
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import gridplot
+
+def entropy_plot(data, title):
+    """Visualise entropy of data with bokeh scatter plot"""
+    plot = figure(title = title)
+    plot.circle([i + 1 for i in range(len(data))], data)
+    return plot
 
 # Create a non-shuffled deck of deck_size cards
 deck_size = 256
 deck = [i for i in range(deck_size)]
 
-def get_entropy(data):
-    """Calculate entropy of data by zlib'bing it"""
-    cmpr = zlib.compressobj(level = 9, wbits = -15, memLevel = 9)
-    return len(cmpr.compress(bytes(data)) + cmpr.flush())
+plots = []
 
-
-ordered_deck_entropy = get_entropy(deck)
+plots.append(entropy_plot(deck, "Ordered deck"))
 
 # To ensure same results on each run
 random.seed(666)
-random_deck_entropy = get_entropy([random.randrange(0, deck_size) for i in range(deck_size)])
-
-def test_entropy(infoText, data):
-    """Test data entropy by compressing it with zlib"""
-    entropy = get_entropy(data)
-    print("{} of {} cards: entropy measure {}, {:.1f}% ordered / {:.1f}% random".format(
-        deck_size, infoText, entropy, entropy / ordered_deck_entropy * 100,
-        entropy / random_deck_entropy * 100))
-
+plots.append(entropy_plot([random.randrange(0, deck_size)
+    for i in range(deck_size)], "random.random()"))
 
 # All zeroes and random numbers (for reference)
-test_entropy("'0'", [0 for i in range(deck_size)])
-# test_entropy(str(deck_size) + " random bytes: ", )
-test_entropy("non-shuffled", deck)
-test_entropy("secrets.randbelow()", [secrets.randbelow(deck_size) for i in range(deck_size)])
+plots.append(entropy_plot([0] * deck_size, "All 0's"))
+# entropy_plot("non-shuffled", deck)
+plots.append(entropy_plot([secrets.randbelow(deck_size)
+    for i in range(deck_size)], "secrets.randbelow()"))
 
 # Put cards into five piles
 # NB: this is not the most efficient code, but I'm writing it to learn different features of Python
@@ -41,11 +37,19 @@ deck5pi = [[], [], [], [], []]
 for card in range(len(deck)):
     deck5pi[card % 5].append(deck[card])
 
-test_entropy("'into five piles'", [deck5pi[p][c] for p in range(5) for c in range(len(deck5pi[p]))])
+plots.append(entropy_plot([deck5pi[p][c] for p in range(5)
+    for c in range(len(deck5pi[p]))], "'into five piles'"))
 
 # Ten piles
 deck10pi = [[], [], [], [], [], [], [], [], [], []]
 for card in range(len(deck)):
     deck10pi[card % 10].append(deck[card])
 
-test_entropy("'into ten piles'", [deck10pi[p][c] for p in range(10) for c in range(len(deck10pi[p]))])
+plots.append(entropy_plot([deck10pi[p][c] for p in range(10)
+    for c in range(len(deck10pi[p]))], "'into ten piles'"))
+
+
+# Show the combined plot!
+output_file("entropy.html")
+combined = gridplot([plots])
+show(combined)
